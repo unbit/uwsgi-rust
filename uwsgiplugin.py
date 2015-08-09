721f6f7916d698f22c9d96ce52cce3773fa514cc
Luca Bruno <lucab@debian.org>
2015-08-09 19:51:52 +0200

    Update script to build rust code via cargo
    Signed-off-by: Luca Bruno <lucab@debian.org>

--- uwsgiplugin.py
+++ uwsgiplugin.py
@@ -5,13 +5,15 @@ import inspect
 base_path = os.path.dirname(inspect.getframeinfo(inspect.currentframe())[0])
 
 NAME = 'rust'
-GCC_LIST = ['rust', '%s/plugin.a' % base_path]
+GCC_LIST = ['rust', '%s/target/release/libuwsgi_rust.a' % base_path]
 
 CFLAGS = []
 
 if os.uname()[0] == 'Darwin':
     CFLAGS.append('-mmacosx-version-min=10.7')
 
-
-if os.system("rustc -o %s/plugin.a --crate-type staticlib %s/plugin.rs" % (base_path, base_path)) != 0:
+if os.system("cargo build --release") != 0:
     os._exit(1)
+
+# To also build the example app:
+#os.system("cargo build --release --manifest-path examples/Cargo.toml")
